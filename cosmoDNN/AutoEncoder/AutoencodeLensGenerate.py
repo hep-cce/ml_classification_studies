308dc5192e40118a705588370a87d4bbe3f0ab52
Nesar Ramachandra <nayaknesar@gmail.com>
2017-08-16 11:36:10 -0500

    cleaning classification module

--- cosmoDNN/AutoEncoder/AutoencodeLensGenerate.py
+++ cosmoDNN/AutoEncoder/AutoencodeLensGenerate.py
@@ -105,79 +105,8 @@ y_val = noiseless_target[num_train:num_files]
 print('x_train shape:', x_train.shape)
 print('y_train shape:', y_train.shape)
 
-# fnames = data_path + DirOutType[1]  #noisy1
-# noisy_data, noisy_target = load_train(fnames)
-# x_train_noisy = noisy_data[0:num_train]
-# y_train_noisy = noisy_target[0:num_train]
-# x_val_noisy = noisy_data[num_train:num_files]
-# y_val_noisy = noisy_target[num_train:num_files]
-
-# plotCheck = False
-# if plotCheck:
-#
-#     fig, ax = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 5))
-#     plt.suptitle(DirOutType[2])
-#
-#     count = 0
-#     np.random.seed(1234)
-#     indx = np.random.randint(20, size = 8)
-#
-#     for ind in indx:
-#         pixel = x_train[ind].reshape(image_size, image_size)
-#         # for i in range(numPlots):
-#         ax[count / 4, count % 4].imshow(pixel, cmap=plt.get_cmap('gray'))
-#         ax[count / 4, count % 4].set_title(str(ind))
-#
-#         count += 1
-#
-#
-#     fig, ax = plt.subplots(2, 4, sharex=True, sharey=True, figsize=(10, 5))
-#     plt.suptitle(DirOutType[1])
-#
-#     count = 0
-#     for ind in indx:
-#         pixel = x_train_noisy[ind].reshape(image_size, image_size)
-#         # for i in range(numPlots):
-#         ax[count / 4, count % 4].imshow(pixel, cmap=plt.get_cmap('gray'))
-#         ax[count / 4, count % 4].set_title(str(ind))
-#
-#         count += 1
-
-# ### Build and train an autoencoder
-
 # this is the size of our encoded representations
 encoding_dim = 64  # 32 floats -> compression of factor 32.6, assuming the input is 2025 floats
-
-def AutoModel_orig():
-
-    # this is our input placeholder
-    input_img = Input(shape=(image_size*image_size,))
-    # "encoded" is the encoded representation of the input
-    encoded = Dense(encoding_dim, activation='relu')(input_img)
-    # "decoded" is the lossy reconstruction of the input
-    decoded = Dense(image_size*image_size, activation='sigmoid')(encoded)
-
-    # this model maps an input to its reconstruction
-    autoencoder = Model(inputs=input_img, outputs=decoded)
-    print("autoencoder model created")
-
-
-    # this model maps an input to its encoded representation
-    encoder = Model(inputs=input_img, outputs=encoded)
-
-    # create a placeholder for an encoded (32-dimensional) input
-    encoded_input = Input(shape=(encoding_dim,))
-
-    # retrieve the last layer of the autoencoder model
-    decoder_layer = autoencoder.layers[-1]
-
-    # create the decoder model
-    decoder = Model(inputs=encoded_input, outputs=decoder_layer(encoded_input))
-
-    adam = Adam(lr=learning_rate, decay=decay_rate)
-    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-    return encoder, decoder, autoencoder
 
 
 def AutoModel_deep():
@@ -221,74 +150,7 @@ def AutoModel_deep():
     return encoder, decoder, autoencoder
 
 
-
-def AutoModel_conv():
-    encoding_dim = 64  # 32 floats -> compression of factor 32.6, assuming the input is 2025 floats
-
-    # this is our input placeholder
-    input_img = Input(shape=(image_size,image_size,1))
-
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    encoded = MaxPooling2D((2, 2), padding='same')(x)
-#    encoded = Dense(encoding_dim, activation='relu')(x)
-
-
-    # at this point the representation is (6, 6, 8) i.e. 288-dimensional
-#    x = Dense(x._keras_shape[1:3], activation='relu')(encoded)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = Conv2D(16, (3, 3), activation='relu')(x)
-    x = UpSampling2D((2, 2))(x)
-    x = ZeroPadding2D( ((1, 0), (1, 0) ) )(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
-
-
-
-    autoencoder = Model(inputs=input_img, outputs=decoded)
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-    # this model maps an input to its reconstruction
-    print("autoencoder model created")
-
-    # this model maps an input to its encoded representation
-    encoder = Model(inputs=input_img, outputs=encoded)
-
-    # create a placeholder for an encoded (32-dimensional) input
-    encoding_dim =  (6,6,8)
-    encoded_input = Input(shape=encoding_dim)
-
-    # retrieve the last layer of the autoencoder model
-    # decoder_layer = autoencoder.layers[-1]
-    deco = autoencoder.layers[-8](encoded_input)
-    deco = autoencoder.layers[-7](deco)
-    deco = autoencoder.layers[-6](deco)
-    deco = autoencoder.layers[-5](deco)
-    deco = autoencoder.layers[-4](deco)
-    deco = autoencoder.layers[-3](deco)
-    deco = autoencoder.layers[-2](deco)
-    deco = autoencoder.layers[-1](deco)
-    # create the decoder model
-    decoder = Model(encoded_input, deco)
-
-
-    # create the decoder model
-    # decoder = Model(inputs=encoded_input, outputs= autoencoder.output  )
-
-    # adam = Adam(lr=learning_rate, decay=decay_rate)
-    # autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-    return encoder, decoder, autoencoder
-
-
-
-
-
-encoder, decoder, autoencoder = AutoModel_conv()
+encoder, decoder, autoencoder = AutoModel_deep()
 # autoencoder.summary()
 
 # Denoising autoencoder
@@ -297,25 +159,3 @@ encoder, decoder, autoencoder = AutoModel_conv()
 ModelFit = autoencoder.fit(x_train, x_train ,
                           batch_size=batch_size, epochs= num_epoch,
                           verbose=2, validation_data=(x_val, x_val))
-
-
-SaveModel = True
-if SaveModel:
-    epochs = np.arange(1, num_epoch+1)
-    train_loss = ModelFit.history['loss']
-    val_loss = ModelFit.history['val_loss']
-
-    training_hist = np.vstack([epochs, train_loss, val_loss])
-
-
-    fileOut = 'Stack_opti' + str(opti_id) + '_loss' + str(loss_id) + '_lr' + str(learning_rate) + '_decay' + str(decay_rate) + '_batch' + str(batch_size) + '_epoch' + str(num_epoch)
-
-    autoencoder.save('../../ModelOutEncode/autoGenerate_' + fileOut + '.hdf5')
-    encoder.save('../../ModelOutEncode/encodeGenerate_' + fileOut + '.hdf5')
-    decoder.save('../../ModelOutEncode/decodeGenerate_' + fileOut + '.hdf5')
-    np.save('../../ModelOutEncode/Generate'+fileOut+'.npy', training_hist)
-
-time_j = time.time()
-print(time_j - time_i, 'seconds')
-
-#-----------------------------------------------------------------------------------
